14daea69d02dd01411fdf93f16f09c76f4707aea
Aeweadserr <108034141+Aeweadserr@users.noreply.github.com>
2024-11-01 16:27:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,102 +1,36 @@
-async function collection(source, target, options = {}) {
+async function collection(source, _target, options = {}) {
     const { config, utils } = options;
-    const { http } = utils;
-    const { fetch, Boxy } = http;
+    const { tauriFetch: fetch } = utils;
+    const { token } = config;
 
-    const { port = 8765 } = config;
-
-    async function ankiConnect(action, version, params = {}) {
-        let res = await fetch(`http://127.0.0.1:${port}`, {
-            method: 'POST',
-            body: Body.json({ action, version, params }),
-        });
-        return res.data;
+    if (token === undefined || token.length === 0) {
+        throw "token not found";
     }
 
-    function ankiText(target) {
-        let result = '';
-        if (typeof target === 'object') {
-            for (let explanation of target.explanations) {
-                result += explanation.trait + '. ';
-                let index = 0;
-                for (let explain of explanation.explains) {
-                    index++;
-                    if (index !== explanation.explains.length) {
-                        result += explain + '; ';
-                    } else {
-                        result += explain + '<br>';
-                    }
-                }
-            }
+    let res = await fetch("https://api.frdic.com/api/open/v1/studylist/words", {
+        method: "POST",
+        headers: { 
+		    'Authorization': token,
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+		},
+        query: {
+            "id": 0, // 单词本 id
+            "language": "en",
+            "words": [source]
+        }
+    });
+
+    if (res.ok) {
+        const result = res.data;
+        if (result.code === 201) {
+            return true;
+        } else if (result.msg) {
+            throw result.msg;
         } else {
-            return target;
+            throw JSON.stringify(result);
         }
-
-        return result;
+    } else {
+        throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(res.data)}`;
     }
-
-    function ankiPronunciation(target) {
-        let results = [];
-        if (typeof target !== 'object' || target.pronunciations === undefined) {
-            return results;
-        }
-        for (let i = 0; i < target.pronunciations.length; i++) {
-            let pronunciation = target.pronunciations[i];
-
-            let region = pronunciation.region;
-            let symbol = pronunciation.symbol;
-
-            // make pronunciation symbol readable
-            region = region ? `[${region}]` : '';
-            symbol = symbol[0] === '/' ? symbol : `/${symbol}/`;
-            let regionSymbol = `${region} ${symbol}`;
-
-            let audio;
-            if (pronunciation.voice) {
-                // step1: convert number array to Char String
-                // step2: convert Char String to base64
-                let voiceString = String.fromCharCode(...pronunciation.voice);
-                let voice = btoa(voiceString);
-
-                let filename = `${region}_${source}.mp3`;
-                let fields = [`Voice${i + 1}`];
-
-                audio = { data: voice, filename, fields };
-            }
-            results.push({ regionSymbol, audio });
-        }
-        return results;
-    }
-
-    await ankiConnect('createDeck', 6, { deck: 'Pot' });
-
-    await ankiConnect('createModel', 6, {
-        modelName: 'Pot Card 2',
-        inOrderFields: ['Front', 'Back', 'Symbol1', 'Voice1', 'Symbol2', 'Voice2'],
-        isCloze: false,
-        cardTemplates: [
-            {
-                Name: 'Pot Card 2',
-                Front: '{{Front}}',
-                Back: '{{FrontSide}}<br>{{Symbol1}} {{Voice1}}<br>{{Symbol2}} {{Voice2}}<hr id=answer>{{Back}}',
-            },
-        ],
-    });
-
-    let pronunciations = ankiPronunciation(target);
-    await ankiConnect('addNote', 6, {
-        note: {
-            deckName: 'Pot',
-            modelName: 'Pot Card 2',
-            fields: {
-                Front: source,
-                Back: ankiText(target),
-                Symbol1: pronunciations[0] && pronunciations[0].regionSymbol,
-                Symbol2: pronunciations[1] && pronunciations[1].regionSymbol,
-            },
-            audio: pronunciations.map((pronunciation) => {
-                return pronunciation.audio;
-            }),
-        },
-    });
 }
